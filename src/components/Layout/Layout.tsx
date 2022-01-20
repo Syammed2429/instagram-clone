9775058caea8761709b8ece22da514e61b2efe0b
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-20 20:35:36 +0530

    created alayout component to display all compinents at once

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Navbar } from "../Navbar/Navbar";
+
+const Layout: FC = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export { Layout };
