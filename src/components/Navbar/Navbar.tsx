6c09e64e54405e4ec7881cdc08b9f353699f5ffc
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-20 20:36:01 +0530

    added a logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,19 @@
 //Importing all the required packages
+import { Container, Flex, Image } from "@chakra-ui/react";
 import React, { FC } from "react";
 
 import logo from "../../assets/images/Logo.svg";
 
 const Navbar: FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Container>
+        <Flex>
+          <Image src={logo} alt="logo" />
+        </Flex>
+      </Container>
+    </>
+  );
 };
 
 export { Navbar };
