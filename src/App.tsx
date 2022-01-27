ba78d11fad8a9436181f23b3bf518c1bbab002e3
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 15:51:23 +0530

    removed css import and deleted css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import { Layout } from "./components/Layout/Layout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 }
 
 export default App;
