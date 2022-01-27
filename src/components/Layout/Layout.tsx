bf55e44ce3b674cde5a5e6d3245b5ec81833d504
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 17:04:43 +0530

    added layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
+
 import { Navbar } from "../Navbar/Navbar";
+import { Stories } from "../Stories/Stories";
 
 const Layout: FC = () => {
   return (
     <>
       <Navbar />
+      <Stories />
     </>
   );
 };
