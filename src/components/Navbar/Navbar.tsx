64aec07278e0a395ec60e988a92c596df64289cf
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 16:32:24 +0530

    added redux

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 //Importing all the required packages
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
 import {
   Box,
   Center,
@@ -12,6 +12,7 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 import { BiSearch } from "react-icons/bi";
+import { useSelector, useDispatch } from "react-redux";
 
 //Importing local files
 import logo from "../../assets/images/Logo.svg";
@@ -21,8 +22,17 @@ import add from "../../assets/images/add.svg";
 import safari from "../../assets/images/safari.svg";
 import like from "../../assets/images/like.svg";
 import userPic from "../../assets/images/user-pic.svg";
+import { searchData } from "../../store/action";
 
 const Navbar: FC = () => {
+  const dispatch = useDispatch();
+  const search = useSelector((state: string) => state.search);
+
+  const searchField = (e: ChangeEvent<HTMLInputElement>) => {
+    const action = searchData(e.target.value);
+    dispatch(action);
+  };
+
   return (
     <>
       <SimpleGrid boxShadow="base" p="3" bg="white">
@@ -35,10 +45,16 @@ const Navbar: FC = () => {
             <Box ml="12rem" mr={20} w={80}>
               <FormControl>
                 <FormLabel htmlFor="search">
-                  <Box position="absolute" top="3" left="30%">
-                    <BiSearch fontSize={20} />
+                  <Box
+                    position="absolute"
+                    top="3"
+                    left={{ base: "25%", md: "30%" }}
+                  >
+                    {search.length === 0 ? <BiSearch fontSize={20} /> : null}
                   </Box>
                   <Input
+                    textAlign="center"
+                    onChange={(e) => searchField(e)}
                     _placeholder={{ textAlign: "center", fontSize: "18px" }}
                     type="text"
                     placeholder="Search"
